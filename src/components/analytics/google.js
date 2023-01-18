ca5544ff02d06b9f229a0d1971c4702d2468496b
ishubs <shubbhu7717@gmail.com>
2023-01-18 17:51:40 +0530

    add event conversion

--- src/components/analytics/google.js
+++ src/components/analytics/google.js
@@ -3,15 +3,16 @@ import Script from 'next/script'
 export const GoogleAnalytics = () => {
     return <><Script async src="https://www.googletagmanager.com/gtag/js?id=AW-718756413"></Script>
         <Script>
-            {`<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-718756413"></script>
-<script>
+            {`
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'AW-718756413');
-</script>
 `}
-        </Script></>
+        </Script>
+        <Script>
+            {`gtag('event', 'conversion', {'send_to': 'AW-718756413/yUMTCOKux4gYEL203dYC'});`}
+        </Script>
+    </>
 }
