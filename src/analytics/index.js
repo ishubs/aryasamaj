3c44bc09ff4d9fd6483bcb9fcaa5b88adc955932
shubham giri <41017859+ishubs@users.noreply.github.com>
2024-10-18 12:08:22 +0530

    add gtags

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -2,7 +2,7 @@ import ReactGA from 'react-ga';
 
 export const initGA = () => {
   console.log('GA init');
-  ReactGA.initialize('UA-xxxxxxxxx-1');
+  ReactGA.initialize('AW-11346454434');
 };
 
 export const logPageView = () => {
